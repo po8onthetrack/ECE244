14dd217e1f52de824b5d1a225fb81794704d07ad
harryz <160258534+po8onthetrack@users.noreply.github.com>
2025-11-02 01:49:10 -0400

    Update QueueList.cpp
    Implemented enqueue and dequeue methods for QueueList, added get_head and get_items functionality.

--- lab4/lab4-release/QueueList.cpp
+++ lab4/lab4-release/QueueList.cpp
@@ -6,25 +6,45 @@ QueueList::QueueList() { head = nullptr; }
 
 QueueList::QueueList(Customer* customer) { head = customer; }
 
-QueueList::~QueueList() {
-  
-}
+QueueList::~QueueList() { }
 
-Customer* QueueList::get_head() {  }
+Customer* QueueList::get_head() { return head; }
 
 void QueueList::enqueue(Customer* customer) {
+  if (head == nullptr) {
+    head = customer; // queue is empty
+  } else {
+    Customer* current = head;
+    while (current->get_next() != nullptr) {
+      current = current->get_next();
+    }
+    current->set_next(customer);
+  }
+
   // a customer is placed at the end of the queue
   // if the queue is empty, the customer becomes the head
-  
 }
 
 Customer* QueueList::dequeue() {
-  // remove a customer from the head of the queue 
+  if (head == nullptr){
+     return nullptr;
+  }
+  Customer* current = head;
+  head = current->get_next();
+  current->set_next(nullptr);
+  return current;
+  // remove a customer from the head of the queue
   // and return a pointer to it
-  
 }
 
 int QueueList::get_items() {
+  int counter = 0;
+  Customer* current = head;
+  while (current != nullptr) {
+    counter += current->get_numOfItems();
+    current = current->get_next();
+  }
+  return counter;
   // count total number of items each customer in the queue has
 }
 
